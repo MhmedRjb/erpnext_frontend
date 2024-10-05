50da3dec5a9a6fa9c11660886a44c90382474b62
MhmedRjb <mohamed2elfateh@gmail.com>
2024-10-05 11:29:23 +0300

    feat:add custom field for icons

--- erpnext_frontend/api.py
+++ erpnext_frontend/api.py
@@ -1,8 +1,13 @@
 import frappe
 from frappe.desk.desktop import get_workspace_sidebar_items
+
+
 @frappe.whitelist(allow_guest=True)
 def get_bootinfo():
-    bootinfo = frappe._dict()
-    bootinfo.sidebar_pages = get_workspace_sidebar_items()
-    allowed_pages = [d.name for d in bootinfo.sidebar_pages.get("pages")]
-    return allowed_pages
+	print("get_bootinfo")
+	print(frappe.session.user)
+	print(frappe.session)
+	bootinfo = frappe._dict()
+	bootinfo.sidebar_pages = get_workspace_sidebar_items()
+	allowed_pages = [d.name for d in bootinfo.sidebar_pages.get("pages")]
+	return allowed_pages
